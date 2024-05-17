91d5d42ae20305b750070ba56758b84eafdb68c9
Anibal Velarde <anibalvelarde@gmail.com>
2024-05-16 23:28:05 -0500

    Added a model to interpret updates for a patient

---
+++ src/Core/BusinessObjects/PatientProfileUpdateRequest.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Neurocorp.Api.Core.BusinessObjects;
+
+public class PatientProfileUpdateRequest
+{
+    public PatientProfileUpdateRequest()
+    {
+        this.FirstName = string.Empty;
+        this.LastName = string.Empty;
+        this.MiddleName = string.Empty;
+        this.Email = string.Empty;
+        this.PhoneNumber = string.Empty;
+        this.Gender = string.Empty;
+        this.DateOfBirth = DateTime.MinValue;
+        this.ActiveStatus = true;
+    }
+
+    public string FirstName { get; set;}
+    public string MiddleName { get; set;}
+    public string LastName { get; set;}
+    public DateTime DateOfBirth { get; set;}
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Gender {get; set;}
+    public bool ActiveStatus { get; set;}
+
+    public override string ToString()
+    {
+        return $"FN: {this.FirstName} MN: {this.MiddleName} LN: {this.LastName} e: {this.Email} p: {this.PhoneNumber} DoB: {this.DateOfBirth.ToShortDateString()} Gender: {this.Gender} Active: {this.ActiveStatus}";
+    }
+
+}
