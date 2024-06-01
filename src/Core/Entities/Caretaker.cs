4c5ce1c89380e235ff245a1cd3553a4276c787ba
Anibal Velarde <anibalvelarde@gmail.com>
2024-06-01 16:02:28 -0500

    Removed legacy column.

--- src/Core/Entities/Caretaker.cs
+++ src/Core/Entities/Caretaker.cs
@@ -4,12 +4,10 @@ public class Caretaker : PersonBase
 {
     public Caretaker()
     {
-        this.Relationship = "";
         this.Patients = [];
         this.Notes = string.Empty;
     }
     public string Notes { get; set; }
-    public string Relationship { get; set; }
     public ICollection<PatientCaretaker> Patients { get; set; }
 
     public UserRole MintNewRole()
